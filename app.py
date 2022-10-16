9321f4302f1ffbb38980bdf6f68d35fcfa8971ff
carolinaquinelato <carolinaqfranchi@hotmail.com>
2022-10-16 15:40:48 -0300

    no columns

--- app.py
+++ app.py
@@ -30,95 +30,89 @@ def main():
 
 	form = st.form(key="annotation")
 
-	with form:
-		col1,col2 = st.columns(2)
-
-		with col1:
-			idade = st.number_input("Idade",1,100, value=54)
-			prog = st.radio("Progesterona", ('Positivo','Negativo'))
-
-		with col2:	
-			estadiamento = st.selectbox("Estadiamento",("IIA","IIB", "IIIA", "IIIB", "IIIC"))
-			estrog = st.radio("Estrogênio", ('Positivo','Negativo'))
-			
-
-		tam_tumor = st.slider("Tamanho do tumor: ", min_value=1, max_value=150, value=24)
-		faixa = st.slider("Escolha quantos anos de visualização:", min_value=3, max_value=10, value=5)
-
-		submitted = st.form_submit_button(label="Submeter")
 	
-		if idade>53:
-			idade =1
-		else:
-			idade=2
+	idade = st.number_input("Idade",1,100, value=54)
+	prog = st.radio("Progesterona", ('Positivo','Negativo'))
 
-		if tam_tumor>24:
-			tam_tumor=1
-		else:
-			tam_tumor=2
-
-		result = {'idade':idade,
-		'estadiamento' : estadiamento,
-		'tam_tumor' : tam_tumor,
-		'estrog' : estrog,
-		'prog' : prog
-		}
-
-		encoded_result = []
-
+	
+	estadiamento = st.selectbox("Estadiamento",("IIA","IIB", "IIIA", "IIIB", "IIIC"))
+	estrog = st.radio("Estrogênio", ('Positivo','Negativo'))
 		
-		neg_pos_map = {"Negativo":0,"Positivo":1}
 
-		def get_value(val,my_dict):
-			for key,value in my_dict.items():
-				if val == key:
-					return value 
+	tam_tumor = st.slider("Tamanho do tumor: ", min_value=1, max_value=150, value=24)
+	faixa = st.slider("Escolha quantos anos de visualização:", min_value=3, max_value=10, value=5)
 
-		def get_other_value(val):
-			estad_map = {"IIA":2,"IIB":2, "IIIA":1, "IIIB":1, "IIIC":1}
-			for key,value in estad_map.items():
-				if val == key:
-					return value 
+	submitted = st.form_submit_button(label="Submeter")
 
-		for i in result.values():
-			if type(i) == int:
-				encoded_result.append(i)
-			elif i in ["Positivo","Negativo"]:
-				res = get_value(i,neg_pos_map)
-				encoded_result.append(res)
-			else:
-				encoded_result.append(get_other_value(i))
+	if idade>53:
+		idade =1
+	else:
+		idade=2
+
+	if tam_tumor>24:
+		tam_tumor=1
+	else:
+		tam_tumor=2
+
+	result = {'idade':idade,
+	'estadiamento' : estadiamento,
+	'tam_tumor' : tam_tumor,
+	'estrog' : estrog,
+	'prog' : prog
+	}
+
+	encoded_result = []
+
+	
+	neg_pos_map = {"Negativo":0,"Positivo":1}
+
+	def get_value(val,my_dict):
+		for key,value in my_dict.items():
+			if val == key:
+				return value 
+
+	def get_other_value(val):
+		estad_map = {"IIA":2,"IIB":2, "IIIA":1, "IIIB":1, "IIIC":1}
+		for key,value in estad_map.items():
+			if val == key:
+				return value 
+
+	for i in result.values():
+		if type(i) == int:
+			encoded_result.append(i)
+		elif i in ["Positivo","Negativo"]:
+			res = get_value(i,neg_pos_map)
+			encoded_result.append(res)
+		else:
+			encoded_result.append(get_other_value(i))
 
 
 	if submitted:
-			single_sample = np.array(encoded_result).reshape(1,-1)
+		single_sample = np.array(encoded_result).reshape(1,-1)
 
-			col1,col2 = st.columns(2)
 
-			with col1:
+		surv = model.predict_survival_function(single_sample, return_array=True)
+		
+		survival = pd.DataFrame({'Probabilidade de Sobrevivência': value for value in surv})
+		survival['Meses'] = survival.index+1
 
-				surv = model.predict_survival_function(single_sample, return_array=True)
-				
-				survival = pd.DataFrame({'Probabilidade de Sobrevivência': value for value in surv})
-				survival['Meses'] = survival.index+1
+		survival = survival.head(faixa*12)
 
-				survival = survival.head(faixa*12)
+		p1 = px.line(survival,x='Meses',y='Probabilidade de Sobrevivência', markers=False, title="Curva de probbilidade de sobrevivência")
+		p1.update_traces(line_color='#666a68')
+		st.plotly_chart(p1)
 
-				p1 = px.line(survival,x='Meses',y='Probabilidade de Sobrevivência', markers=False, title="Curva de probbilidade de sobrevivência")
-				p1.update_traces(line_color='#666a68')
-				st.plotly_chart(p1)
+	
+		surv2 = model.predict_cumulative_hazard_function(single_sample, return_array=True)
 
-			with col2:
-				surv2 = model.predict_cumulative_hazard_function(single_sample, return_array=True)
+		hazard = pd.DataFrame({'Hazard Acumulado': value for value in surv2})
+		hazard['Meses'] = hazard.index+1
 
-				hazard = pd.DataFrame({'Hazard Acumulado': value for value in surv2})
-				hazard['Meses'] = hazard.index+1
+		hazard = hazard.head(faixa*12)	
 
-				hazard = hazard.head(faixa*12)	
-
-				p2 = px.line(hazard,x='Meses',y='Hazard Acumulado', markers=False, title="Predição da função de Hazard acumulada")
-				p2.update_traces(line_color='#666a68')
-				st.plotly_chart(p2)
+		p2 = px.line(hazard,x='Meses',y='Hazard Acumulado', markers=False, title="Predição da função de Hazard acumulada")
+		p2.update_traces(line_color='#666a68')
+		st.plotly_chart(p2)
 			
 if __name__ == "__main__":
     main()
